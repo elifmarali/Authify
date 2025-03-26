947e2fa67789022a53964c0b2ba1e0bb73943ebf
Elif Maralı <71133148+elifmarali@users.noreply.github.com>
2025-03-26 14:17:43 +0300

    BugFix - Home page left image düzenlendi

--- app/container/HomeMain/index.jsx
+++ app/container/HomeMain/index.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 
 function HomeMain() {
   return (
-    <div className={`!w-[45%] rounded-3xl bg-white h-[36rem] !p-6 !m-10 opacity-95 ${styles.homeMain}`}>
+    <div className={`!w-[40%] rounded-3xl bg-white h-[36rem] !p-6 !m-10 opacity-95 ${styles.homeMain}`}>
       <LeftSection />
     </div>
   );
