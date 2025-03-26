658dab070a541d23c385a81ab642620b4ac185d1
Elif Maralı <71133148+elifmarali@users.noreply.github.com>
2025-03-26 13:58:31 +0300

    Dev - Home sayfasına header eklendi

--- app/container/Home/index.jsx
+++ app/container/Home/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Banner from "../../component/Banner";
 import Grid from "@mui/material/Grid2";
 import HomeMain from "../HomeMain";
+import Header from "@/app/component/Header";
 
 function HomeContainer() {
   return (
     <Grid className="h-full">
+      <Header />
       <Banner />
       <HomeMain />
     </Grid>
