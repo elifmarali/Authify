84c9d9223af30712b96e4bbd258d649c8f9b8846
Elif Maralı <71133148+elifmarali@users.noreply.github.com>
2025-03-26 14:00:16 +0300

    BugFix - Header hatası düzenlendi

--- app/container/Home/index.jsx
+++ app/container/Home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Banner from "../../component/Banner";
 import Grid from "@mui/material/Grid2";
 import HomeMain from "../HomeMain";
-import Header from "@/app/component/Header";
+import Header from "../../component/Header";
 
 function HomeContainer() {
   return (
